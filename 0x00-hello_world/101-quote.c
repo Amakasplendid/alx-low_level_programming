4f1f9307437452e5d1d89b263b8f49a114fe025d
Amakasplendid <chiamakaonuora@gmail.com>
2023-06-16 12:50:12 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdioo.h>
+#include <unistd.h>
+/**
+ * main - Prints a line to standard error
+ * Return: Always 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
