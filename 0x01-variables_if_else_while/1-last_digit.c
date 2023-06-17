e156a6feb2053311def6a6870e09f18d98383aab
Amakasplendid <chiamakaonuora@gmail.com>
2023-06-16 17:04:02 -0700

    Alphabets in upper and lower cases

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - To determine the last digits
  * Return: 0
  * /
-
 int  main(void)
 {
-	int n. last_d;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_d = n % 10;
 
-	if (last_d  > 5)
+	if ((n % 10)  > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	}
-	else if (last_d == 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and is  0\n", n, last_d);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	printf("Last digit of %d is %d and is 0\n" n, n % 10);
 	}
 		
 	return (0);
