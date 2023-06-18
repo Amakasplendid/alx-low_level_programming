0dc2024f934c757f866a9abb6f869a1ab9a45578
Amakasplendid <chiamakaonuora@gmail.com>
2023-06-18 05:53:51 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i <= 9)
+	{
+		putchar("%i, i");
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
