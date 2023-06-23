83ea798de3630aea9c8b34ae24d88786c3053f79
Amakasplendid <chiamakaonuora@gmail.com>
2023-06-23 14:51:00 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: - The 1st integer
+ * @b: The 2nd integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 int _putchar(char c);
 void print_square(int size);
 int _isupper(int c);
+int mul(int a, int b);
 #endif
