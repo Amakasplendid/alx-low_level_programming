e557a367e30b659448b305b78cf03cb4d29bdaaa
Amakasplendid <chiamakaonuora@gmail.com>
2023-06-26 16:24:45 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - function for random program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int pass[100];
+	int x, sum, y;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
+	{
+		pass[x] = rand() % 78;
+		sum += (pass[x] + '0');
+		putchar(pass[x] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+			putchar(y + '0');
+			break;
+		}
+	}
+}
+
