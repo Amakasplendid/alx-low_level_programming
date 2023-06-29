9ff22f9df21096a013ad05a3704d800646138993
Amakasplendid <chiamakaonuora@gmail.com>
2023-06-29 10:55:02 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int x;
 	int y;
-	
+
 	x = 0;
 	while (dest[x] != '\0')
 	{
@@ -19,11 +19,10 @@ char *_strncat(char *dest, char *src, int n)
 	y = 0;
 	while (y < n && src[y] != '\0')
 	{
-		dest[x] = src[y];
-		x++;
-		y++;
+	dest[x] = src[y];
+	x++;
+	y++;
 	}
 	dest[x] = '\0';
-	return (dest);	
-
+	return (dest);
 }
