173789ff45beb64fe83a01d87cdab223307ca18f
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-04 14:25:22 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * *str_concat - concatenates string
+ * @s1: first string
+ * @*s2: second string
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+}
