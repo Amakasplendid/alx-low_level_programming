e6caffeca6252a5b64fcacded44551393381f436
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-10 12:48:13 -0700

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *new_dog - new function
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
