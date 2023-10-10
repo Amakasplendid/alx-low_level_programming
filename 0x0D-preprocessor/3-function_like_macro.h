ee7efa61c00457e6681594059e43e66c29770cca
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-10 06:36:25 -0700

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,8 +2,8 @@
 #define ABS_H
 /**
  * file: 3-function_like_macro.h
- *
+ * @x: input value
  * desc: absolute value of input
  */
-#define ABS(x) ((x < 0) ? -(1) * (x) : (x))
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
 #endif
