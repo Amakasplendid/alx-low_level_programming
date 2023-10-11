9fe761b798599baf2bc405fcba89a3bcc6396b19
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-11 13:30:24 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,15 +6,10 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 #endif
