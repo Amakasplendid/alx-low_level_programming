a29ff7a27bf8ceadd00990de3977be2141bccd1b
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-19 06:34:08 -0700

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - number of elements in linked list
+ * @h: pointer
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int a;
+
+	if (h == NULL)
+		return (0);
+	for (a = 0; h->next != NULL; a++)
+		h = h->next;
+	return (a);
+}
+
