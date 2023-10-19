e6c74dbc60d4e799aa050ca2e358e8e099c304ac
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-19 07:32:08 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,18 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 		return (0);
+	i
 	for (a = 1; h->next != NULL; a++)
 	{
 		if (h->str == NULL)
 			printf("[%u] %s\n", h->len, "(nil)");
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
+		
 	}
-	printf("[%u] %s\n", h->len, h->str));
+	printf("[%u] %s\n", h->len, "(nil)");
 	return (a);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -13,7 +13,8 @@ size_t list_len(const list_t *h)
 	int a;
 
 	if (h == NULL)
-		return (0);
+	return (0);
+
 	for (a = 0; h->next != NULL; a++)
 		h = h->next;
 	return (a);
