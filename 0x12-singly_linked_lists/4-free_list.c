8cfde5eee7a2e83eb8f3b04d37db25620ba0e24f
Amakasplendid <chiamakaonuora@gmail.com>
2023-10-21 11:22:08 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: pointer
+ * Return: none
+ */
+void free_list(list_t *head)
+{
+	 list_t *tep;
+
+	 if (head == NULL)
+		 return;
+	 while (head->next != NULL)
+	 {
+		 tep = head->next;
+		 free(head->str);
+		 free(head);
+		 head = tep;
+	 }
+	 free(head->str);
+	 free(head);
+}
